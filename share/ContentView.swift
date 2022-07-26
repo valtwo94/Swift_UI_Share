cc48785e9d5bbf33e34ede32b4d1f1491e5abf5d
zebra <zebra@zebraui-iMac.local>
2022-07-26 22:11:50 +0900

    initial commit

---
+++ share/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  share
+//
+//  Created by zebra on 2022/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ share/shareApp.swift
@@ -0,0 +1,17 @@
+//
+//  shareApp.swift
+//  share
+//
+//  Created by zebra on 2022/07/26.
+//
+
+import SwiftUI
+
+@main
+struct shareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
